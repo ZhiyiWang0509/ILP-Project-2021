62dd1e38dd4cc3e6255ffb3944c8f68a79f3ff5d
wangz <wangzhiyiamy@outlook.com>
2021-09-27 11:51:50 +0100

    Defined the "isConfined" method in the LongLat class. The method passes the relevant Junit tests in the AppTest file.

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        LongLat appletonTower = new LongLat(-3.191594,55.943658);
+        System.out.println(appletonTower.isConfined());
+
     }
 }
--- src/main/java/uk/ac/ed/inf/LongLat.java
+++ src/main/java/uk/ac/ed/inf/LongLat.java
@@ -1,16 +1,30 @@
 package uk.ac.ed.inf;
 
 public class LongLat {
+    // two public fields in the class
     public double longitude;
     public double latitude;
 
+    /* the four corners of the confined area
+       moves beyond the four locations are not allowed.
+
+    public final LongLat forrestHill = new LongLat(-3.192473,55.946233);
+    public final LongLat kfc = new LongLat(-3.184319,55.946233);
+    public final LongLat topMeadows = new LongLat(-3.192473,55.942617);
+    public final LongLat buccleuchBusStop = new LongLat(-3.184319,55.942617);
+    */
+
+    // the constructor of the class LongLat has longitude and latitude as inputs.
     public LongLat(double longitude, double latitude) {
         this.longitude = longitude;
         this.latitude = latitude;
     }
 
+    // method to check if the drone is within the confined area
     public boolean isConfined() {
-        return true;
+        boolean latCheck = (latitude < 55.946233) && (latitude > 55.942617);
+        boolean longCheck = (longitude > -3.192473) && (longitude < -3.184319);
+        return (latCheck && longCheck);
     }
 
     public double distanceTo(LongLat businessSchool) {
