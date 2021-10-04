9b95d808df7c020e78d2412589ab87eb72e1b6cb
wangz <wangzhiyiamy@outlook.com>
2021-10-04 19:39:02 +0100

    The Resturant class act as a parser for the json file dragged from the website. The getItemsHash() method would generate a hash map for the items on the menu of each restaurant. In the Menus class, the getItemList() method would call getItemHash() on each Resturant object from the json list object, which would eventually generate a big hashmap of all the items on sale. The big hashmap is the key for the following getDeliveryCost() method. In getDeliveryCost, the item name input will be pass to the hashmap as key to search for the corresponding value of the key which is the price of the item. Finally, we get the total delivery cost by adding the price of all the items and 50 which is the delivery cost.

--- src/main/java/uk/ac/ed/inf/Menus.java
+++ src/main/java/uk/ac/ed/inf/Menus.java
@@ -6,6 +6,7 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Menus {
 
@@ -18,30 +19,43 @@ public class Menus {
         this.port = port;
     }
 
+    // The method would extract items from every restaurant as a hashmap with the item name as the key and item price as the value
+    // Hashmap is considered since it's a quick way to lookup price of item.
+    public HashMap<String, Integer> getItemList(){
+        WebAccess newAccess = new WebAccess(server, port,"menus","menus");
+
+        // create an arrayList contain restaurants information as json object
+        Type listType = new TypeToken<ArrayList<Resturant>>(){}.getType();
+        ArrayList<Resturant> resturantList = new Gson().fromJson(String.valueOf(newAccess.getResponse()), listType);
+
+
+        HashMap<String, Integer> items = new HashMap<>();
+        for (Resturant resturant : resturantList){
+            HashMap<String, Integer> localItems = resturant.getItemsHash(); // generate a hashmap using the getItemHash() method from the restaurant class for each restaurant
+            items.putAll(localItems);  // combine the information of each to a large hashmap
+        }
+        return items;
+    }
+
     public int getDeliveryCost(String item1) {
-        return 0;
+        HashMap<String, Integer> itemList = getItemList();
+        return itemList.get(item1) + 50;
     }
 
     public int getDeliveryCost(String item1, String item2) {
-        return 0;
+        HashMap<String, Integer> itemList = getItemList();
+        return (itemList.get(item1) + itemList.get(item2) + 50);
     }
 
     public int getDeliveryCost(String item1, String item2, String item3) {
-        return 0;
+        HashMap<String, Integer> itemList = getItemList();
+        return (itemList.get(item1) + itemList.get(item2) + itemList.get(item3) + 50);
     }
 
     public int getDeliveryCost(String item1, String item2, String item3, String item4) {
-        return 0;
+        HashMap<String, Integer> itemList = getItemList();
+        return (itemList.get(item1) + itemList.get(item2) + itemList.get(item3)+ itemList.get(item4) + 50);
     }
 
-    public static void main(String[] args){
-        Menus menu1 = new Menus("localhost","80");
-        WebAccess newAccess = new WebAccess(menu1.server, menu1.port,"menus","menus");
-        Type listType = new TypeToken<ArrayList<Resturant>>(){}.getType();
-        ArrayList<Resturant> resturantList = new Gson().fromJson(String.valueOf(newAccess.getResponse()), listType);
-        System.out.println(resturantList);
-        //System.out.println(newAccess.getResponse());
 
-
-    }
 }
--- src/main/java/uk/ac/ed/inf/Resturant.java
+++ src/main/java/uk/ac/ed/inf/Resturant.java
@@ -1,19 +1,29 @@
 package uk.ac.ed.inf;
 
 
+import java.util.HashMap;
 import java.util.List;
-import com.google.gson.Gson;
+
 
 // Parsed class from menus.json
 public class Resturant {
 
-    private String name;
-    private String location;
-    private List<MenuDetails> menu;
+    String name;
+    String location;
+    List<MenuDetails> menu;
 
     public static class MenuDetails {  // the Menu inner class is for each single item
-        private String item;
-        private int pence;
+        String item;
+        Integer pence;
     }
 
+    // the method would generate a hash map of all the items in the restaurant's menu
+    // for a quicker lookup of the price.
+    public HashMap<String, Integer> getItemsHash(){
+        HashMap<String, Integer> itemList = new HashMap<>();
+        for (MenuDetails singleItem : menu){
+            itemList.put(singleItem.item, singleItem.pence);
+        }
+        return itemList;
+    }
 }
