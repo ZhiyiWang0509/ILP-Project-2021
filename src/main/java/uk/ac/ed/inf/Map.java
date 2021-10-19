3b13198b76deec2c6a1ec6cbb5754b533fec1c95
wangz <wangzhiyiamy@outlook.com>
2021-10-20 00:31:35 +0100

    Removed the redundent dependency in the pom.xml Imported relavant packages in the Map class

--- src/main/java/uk/ac/ed/inf/Map.java
+++ src/main/java/uk/ac/ed/inf/Map.java
@@ -1,5 +1,9 @@
 package uk.ac.ed.inf;
 
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.Polygon;
+
 // Class to deal with geojson files include methods related to non-fly zone and landmarks
 public class Map {
 }
